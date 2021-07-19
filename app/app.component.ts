05ed489396e1ce7c5e869e50f3afdca92b1e8957
alfonsusenrico <enrico.soebijanto@gmail.com>
2021-07-19 15:24:40 +0700

    finalized home page, added contact icon

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,7 +9,6 @@ export class AppComponent {
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(e: any) {
-    console.log(e);
     const el = document.getElementById('bg');
     const x = (window.innerWidth + e.pageX*-7)/100;
     const y = (window.innerHeight + e.pageY*-7)/100;
@@ -17,21 +16,64 @@ export class AppComponent {
   }
 
   async ngAfterViewInit() {
-    console.log(screen);
-    //await this.hidden();
-    //await this.renderGreet();
-    //await this.renderHiddenGreet();
-    // const el = document.getElementById("tes")
-    // if(el) {
-    //   console.log(screen);
-    //   console.log("translate("+screen.height+"px,"+screen.width+"px)")
-    //   el.style.transform = "translateX(calc(100vw - 100%))";
-    //   //el.style.left = "100%";
-    // }
+    await this.setNav();
   }
 
   async sleep(ms: number) {
     return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  async setNav() {
+    var url = window.location.href
+    var href = url.split('#')[1];
+    if(!href || href == 'home') {
+      return;
+    }
+    else {
+      console.log(href);
+      var current = href+'Nav';
+      var baseElement = document.getElementById('homeNav')?.classList;
+      var currentElement = document.getElementById(current)?.classList;
+    
+    baseElement?.remove('navActive');
+    baseElement?.add('navInactive');
+
+    currentElement?.remove('navInactive');
+    currentElement?.add('navActive');
+    }
+  }
+
+  toggleNav(id: string) {
+    var url = window.location.href;
+    var href = url.split('#')[1];
+    if(!href) {
+      href = 'home';
+    }
+    var current = href+'Nav';
+
+    var currentElement = document.getElementById(current)?.classList;
+    var nextElement = document.getElementById(id)?.classList;
+
+    currentElement?.remove('navActive');
+    currentElement?.add('navInactive');
+
+    nextElement?.remove('navInactive');
+    nextElement?.add('navActive');
+
+    // var el = document.getElementById(id)?.classList;
+    // if (el?.contains('navActive')) {
+    //   el.remove('navActive');
+    //   el.add('navInactive');
+    //   el?.forEach(elements => {
+    //     elements.
+    //   })
+    //   return;
+    // }
+    // if (el?.contains('navInactive')) {
+    //   el.remove('navInactive');
+    //   el.add('navActive');
+    //   return;
+    // }
   }
 
 }
