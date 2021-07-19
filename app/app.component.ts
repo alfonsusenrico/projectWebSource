4eb91dfa1750003a1282ca32ff0d873f0e1a5645
alfonsusenrico <enrico.soebijanto@gmail.com>
2021-07-19 23:56:22 +0700

    start working for about section

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +9,73 @@ import { Component, ViewChild, ElementRef, HostListener } from '@angular/core';
 })
 export class AppComponent {
 
+  constructor(public router: Router, private scroller: ViewportScroller, private route: ActivatedRoute) {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  }
+
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(e: any) {
     const el = document.getElementById('bg');
     const x = (window.innerWidth + e.pageX*-7)/100;
     const y = (window.innerHeight + e.pageY*-7)/100;
     el!.style.transform = `translate(${x+2000}px, ${y}px)`;
+  }
+
+  @HostListener('wheel', ['$event'])
+  async scroll(e: WheelEvent) {
+    const url = window.location.href;
+    var href = url.split('#')[1];
+
+    var current = '';
+
+    //scroll down
+    if(e.deltaY > 0) {
+      if(!href) {
+        href = 'home';
+      }
+      current = href+'Nav';
+      var next = '';
+      switch(href) {
+        case 'home':
+          next = 'about';
+          break;
+        case 'about':
+          next = 'projects';
+          break;
+        case 'projects':
+          return;
+      }
+    }
+    //scroll up
+    else {
+      if(!href) {
+        return;
+      }
+      current = href+'Nav';
+      var next = '';
+      switch(href) {
+        case 'home':
+          return;
+        case 'about':
+          next = 'home';
+          break;
+        case 'projects':
+          next = 'about';
+          break;
+      }
+    }
+
+    var currentElement = document.getElementById(current)?.classList;
+    var nextElement = document.getElementById(next+'Nav')?.classList;
+    
+    currentElement?.remove('navActive');
+    currentElement?.add('navInactive');
+
+    nextElement?.remove('navInactive');
+    nextElement?.add('navActive');
+
+    this.router.navigateByUrl("#"+next);
+    this.scroller.scrollToAnchor(next);
   }
 
   async ngAfterViewInit() {
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
